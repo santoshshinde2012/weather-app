5d21fdc3aca870676581d406646ea43a9e92e9cd
santosh <santosh.shinde@sixfactors.ca>
2017-11-19 16:18:37 +0530

    add favicon

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const express=require('express');
-const path=require('path');
-const port=process.env.PORT || 8080;
-const app=express();
+let express = require('express');
+let path = require('path');
+let favicon =  require('serve-favicon');
+
+let port = process.env.PORT || 8080;
+
+let app = express();
 
 app.use(express.static(__dirname));
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+
 
 app.get('*',(req,res)=>{
   res.sendFile(path.resolve(__dirname,'index.html'))
 });
+
 app.listen(port);
+
 console.log("server stated on port ", port)
